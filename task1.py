27a5cd7fece78c58e4df29c9ab66633632ee5040
Pa <past80@gmail.com>
2024-02-15 15:19:34 +0400

    Task 1 - fixed wrong Bernoulli formula

--- task1.py
+++ task1.py
@@ -7,16 +7,22 @@ def Fact(n):
     return n * Fact(n - 1)
 
 def Bernoulli(n, m, p):
-  return comb(n,m) * p ** n * (1-p) ** (n-m)
+  result = comb(n,m) * p ** m * (1-p) ** (n-m)
+  print ("--вероятность по формуле Бернулли=", result)
+  return result
 
 p_ = 0.95 # p - вероятность что ручка исправная
 n = m = 5 # m - требуемое кол-во исправных ручек
 Pmax = 0.99 # Pmax - заданная вероятность вытащить m исправных ручек
 
-Psum = 0  # Psum - текушая вероятность вытащить m исправных ручек в n испытаниях
-while Psum < Pmax:
+while True :
+  print("Испытание при n =", n)
+  Psum = 0.0  # Psum - текушая вероятность вытащить m исправных ручек в n испытаниях
   for i in range(m, n+1):
-    Psum += Bernoulli(i, m, p_)
+    Psum += Bernoulli(n, i, p_)
+    print("- суммарная вероятность при m =", i, "равна =", Psum)
+  if Psum >= Pmax:
+    break
   n += 1
 
-print("Ответ 1: Требуется вытащить n =", n, "ручек." )
+print("Ответ 1: Требуется вытащить n =", n, "ручек с вероятностью", Psum)
